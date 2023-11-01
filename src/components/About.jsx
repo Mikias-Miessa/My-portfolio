a20d614ff584de0e17ade73ed9931b7aeee3d61a
Mikias Miessa <mikiasmiessa@gmail.com>
2023-11-01 23:06:19 +0300

    navbar, home and about sections are done

---
+++ src/components/About.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name='about' className='w-full bg-gradient-to-b from-indigo-950 to-black text-white'>
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='pt-4'>
+          <p className='text-4xl font-bold inline border-b-4'>About</p>
+        </div>
+
+        <p className='text-lg font-light mt-20'>
+          Hello, my name is Mikias Miessa and I'm a passionate Full Stack Developer with expertise in both Frontend and Backend Technologies.
+          I have hands-on experience with HTML, CSS, JavaScript, TypeScript, ReactJS, VueJS, Ant Design, Material UI, NextJS, wordPress, NodeJS, Express, Django, and databases such as MongoDB and MySQL.
+          I also have experience with React Native for mobile app development.
+          I am a firm believer in Agile Methodology and have collaborated on projects utilizing these technologies, delivering top-notch solutions that have exceeded client expectations.
+          I am always eager to learn and grow, and am excited to be part of a team that is dedicated to creating innovative solutions that make a positive difference in people's lives.
+        </p>
+
+        <br />
+
+        <p className='text-lg font-light'>
+          I am a highly ambitious, self-motivated and driven software engineer with a passion for innovation and problem-solving.
+          I graduated from Addis Ababa Science and Technology University with a degree in Computer Engineering, where I maintained a 3.73 GPA.
+          I am always seeking new challenges and opportunities to learn and grow.
+          I am confident that I have the skills and experience necessary to be successful in any role I am given.
+          Let's build something great together!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default About
