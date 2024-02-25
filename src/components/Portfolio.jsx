b2510639eacb58e878acbc1dd5cb7abddfa69e73
Mikias Miessa <mikiasmiessa@gmail.com>
2024-02-25 18:47:01 +0300

    add new project

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from 'react';
 // import second from 'first'
-import TmhirtBete from '../assets/portfolio/TmhirtBete.png'
-import FoodApp from '../assets/portfolio/FoodApp.png'
-import Inventory from '../assets/portfolio/Inventory.jpg'
-import SmartWaste from '../assets/portfolio/SmartWaste.png'
-import LMS from '../assets/portfolio/LMS.png'
-import TweeterClone from '../assets/portfolio/TweeterClone.png'
+import TmhirtBete from '../assets/portfolio/TmhirtBete.png';
+import FoodApp from '../assets/portfolio/FoodApp.png';
+import Inventory from '../assets/portfolio/Inventory.jpg';
+import SmartWaste from '../assets/portfolio/SmartWaste.png';
+import LMS from '../assets/portfolio/LMS.png';
+import Gobeze from '../assets/portfolio/gobeze.png';
+import TweeterClone from '../assets/portfolio/TweeterClone.png';
 
-import { motion, useAnimation } from "framer-motion";
+import { motion, useAnimation } from 'framer-motion';
 import { useEffect, useState } from 'react';
 
-
 const Portfolio = () => {
-   const controls = useAnimation();
+  const controls = useAnimation();
   const [inView, setInView] = useState(false);
 
   useEffect(() => {
@@ -37,57 +37,69 @@ const Portfolio = () => {
     }
   }, [inView, controls]);
 
-   const portfolios = [
+  const portfolios = [
     {
-      id:1,
-       src: TmhirtBete,
-      title:'Timhirt Bete App',
-      demo: 'https://t.me/temhrtbete',
-      code: ''
+      id: 7,
+      src: Gobeze,
+      title: 'Gobeze Learning',
+      demo: 'https://gobezelearning.vercel.app/',
+      code: '#',
     },
     {
-      id:2,
-      src: FoodApp,
-      title:'Food Delivery App',
-      demo: '#',
-      code: 'https://github.com/Mikias-Miessa/Tikus-app'
-    },
-    {
-      id:3,
+      id: 3,
       src: SmartWaste,
-      title:'Smart Waste Mangement System Using MQTT',
+      title: 'Smart Waste Mangement System Using MQTT',
       demo: '#',
-      code: 'https://github.com/Mikias-Miessa/Smart-bin-front-and-back-end'
+      code: 'https://github.com/Mikias-Miessa/Smart-bin-front-and-back-end',
     },
     {
-      id:4,
+      id: 1,
+      src: TmhirtBete,
+      title: 'Timhirt Bete App',
+      demo: 'https://t.me/temhrtbete',
+      code: '#',
+    },
+    {
+      id: 2,
+      src: FoodApp,
+      title: 'Food Delivery App',
+      demo: '#',
+      code: 'https://github.com/Mikias-Miessa/Tikus-app',
+    },
+
+    {
+      id: 4,
       src: Inventory,
-      title:'QR Code Based Inventory Management System',
+      title: 'QR Code Based Inventory Management System',
       demo: 'https://mikiasmiessa.000webhostapp.com/inventory/Products.php',
-      code: 'https://github.com/Mikias-Miessa/QRcode-based-inventory-management-system'
+      code: 'https://github.com/Mikias-Miessa/QRcode-based-inventory-management-system',
     },
     {
-      id:5,
+      id: 5,
       src: TweeterClone,
-      title:'Social Media',
+      title: 'Social Media',
       demo: '#',
-      code: 'https://github.com/Mikias-Miessa/Twitter-clone-django'
+      code: 'https://github.com/Mikias-Miessa/Twitter-clone-django',
     },
     {
-      id:6,
+      id: 6,
       src: LMS,
-      title:'Library Management System',
+      title: 'Library Management System',
       demo: '#',
-      code: 'https://github.com/Mikias-Miessa/Library-Management-System'
+      code: 'https://github.com/Mikias-Miessa/Library-Management-System',
     },
-   
-  ]
- return (
-    <div name='portfolio' className='bg-gradient-to-b from-black via-black to-indigo-950 w-full text-white pt-20'>
+  ];
+  return (
+    <div
+      name='portfolio'
+      className='bg-gradient-to-b from-black via-black to-indigo-950 w-full text-white pt-20'
+    >
       {/* md:h-screen */}
       <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
-          <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
+          <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
+            Portfolio
+          </p>
           <p className='py-6'>Check out some of my work right here</p>
         </div>
 
@@ -107,15 +119,44 @@ const Portfolio = () => {
                 }}
                 viewport={{ once: true, amount: 0.8 }}
               >
-                <img src={port.src} alt={port.src} className='rounded-md duration-200 hover:scale-150' />
-                <div className='flex justify-center items-center pt-5 font-semibold px-10'>{ port.title}</div>
+                <img
+                  src={port.src}
+                  alt={port.src}
+                  className='rounded-md duration-200 hover:scale-150'
+                />
+                <div className='flex justify-center items-center pt-5 font-semibold px-10'>
+                  {port.title}
+                </div>
                 <div className='flex items-center justify-center'>
-
-                  <a href={port.demo} className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105' target='_blank' rel='noreferrer'>
-                    <motion.button className="text-indigo-500" whileHover={{ textShadow: "8px,8px,20px,rgb(255,255,255)" }}>Demo</motion.button>
+                  <a
+                    href={port.demo}
+                    className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <motion.button
+                      className='text-indigo-500'
+                      whileHover={{
+                        textShadow: '8px,8px,20px,rgb(255,255,255)',
+                      }}
+                    >
+                      Demo
+                    </motion.button>
                   </a>
-                  <a href={port.code} className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105' target='_blank' rel='noreferrer'>
-                    <motion.button className="text-indigo-500" whileHover={{textShadow:"8px,8px,8px,rgb(255,255,255)"}}>Code</motion.button>
+                  <a
+                    href={port.code}
+                    className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <motion.button
+                      className='text-indigo-500'
+                      whileHover={{
+                        textShadow: '8px,8px,8px,rgb(255,255,255)',
+                      }}
+                    >
+                      Code
+                    </motion.button>
                   </a>
                 </div>
               </motion.div>
@@ -125,6 +166,6 @@ const Portfolio = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Portfolio
+export default Portfolio;
